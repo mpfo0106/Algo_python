dd8526903f40658db394ae8da31c5bdbd4e690fa
mpfo0106 <mpfo0106@gmail.com>
2023-11-15 16:00:35 +0900

    SWEA_1206_1일차_View

--- swea/SWEA_1206_1일차_View.py
+++ swea/SWEA_1206_1일차_View.py
@@ -1,7 +1,11 @@
-# 
+for i in range(1, 11):  # 테스트 케이스 10개
+    N = int(input())
+    heights = list(map(int, input().split()))
+    house = 0
 
-import sys
-
-for i in range(10):
-    heights = sys.stdin.readline().split(" ")
-
+    for idx in range(2, N - 2):
+        max_height = max(heights[idx - 2], heights[idx - 1], heights[idx + 1], heights[idx + 2])
+        capa = heights[idx] - max_height
+        if capa > 0:
+            house += capa
+    print(f'#{i} {house}')
