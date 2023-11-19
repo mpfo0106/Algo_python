278f0c259f5d12b8ab8849d40886262946915c64
mpfo0106 <mpfo0106@gmail.com>
2023-11-19 21:16:36 +0900

    bfs_미로탈출

--- this_is_coding_test/dfs_bfs/미로탈출.py
+++ this_is_coding_test/dfs_bfs/미로탈출.py
@@ -1,7 +1,32 @@
-# bfs. 1만 밟아서 탈출
-N,M = map(int,input().split(" "))
+# 0 회
+from collections import deque
+
+N, M = map(int, input().split(" "))
 graph = []
 for i in range(N):
-    line = list(map(int,input().split(" ")))
-    for j in range(M):
-        graph.append(line[j])
+    graph.append(list(map(int, input())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= N or ny >= M:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[N - 1][M - 1]
+
+
+print(bfs(0, 0))
