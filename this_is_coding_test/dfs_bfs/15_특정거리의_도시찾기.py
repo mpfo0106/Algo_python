e52fcc78db5108410e9b20597228ccbab4dda960
mpfo0106 <mpfo0106@gmail.com>
2023-11-22 00:20:35 +0900

    15_특정거리의 도시찾기

--- this_is_coding_test/dfs_bfs/15_특정거리의_도시찾기.py
+++ this_is_coding_test/dfs_bfs/15_특정거리의_도시찾기.py
@@ -1,33 +1,75 @@
-# X 부터 출발한 도시 중, 최단거리가 정확히 K인 모든 도시들의 번호를 출력
-# 여러 상태를 봐야함으로 bfs 다.
+# # X 부터 출발한 도시 중, 최단거리가 정확히 K인 모든 도시들의 번호를 출력
+# # 최단경로 문제. 다익스트라 접근.
+#
+# import sys
+# import heapq
+#
+# INF = int(1e9)
+# # 도시 n , 도로 m , 거리 k, 출발 x
+# n, m, k, x = map(int, sys.stdin.readline().split(" "))
+# graph = [[] for _ in range(n + 1)]
+# distance = [INF] * (n + 1)
+# for _ in range(m):
+#     a, b = map(int, sys.stdin.readline().split(" "))
+#     graph[a].append((1, b))  # 가중치 1
+#
+#
+# def dijkstra(start):
+#     q = []
+#     heapq.heappush(q, (0, start))
+#     distance[start] = 0
+#     while q:
+#         dist, now = heapq.heappop(q)
+#         if distance[now] < dist:  # 이미 처리되었다면(거리가 짧아진거니)
+#             continue
+#         for i in graph[now]:
+#             cost = dist + i[0]
+#             if cost < distance[i[1]]:
+#                 distance[i[1]] = cost
+#                 heapq.heappush(q, (cost, i[1]))
+#
+#
+# ans = []
+# dijkstra(x)
+# for i in range(1, n + 1):
+#     if distance[i] == k:
+#         ans.append(str(i))
+#
+# if not ans:
+#     ans.append(str(-1))
+#
+# print("\n".join(ans))
 
-# x 에서 출발해 거리가 k 가 되는걸 찾아야해
-# queue 에 넣을때
-import sys
+# TODO BFS 로 푼것
+
 from collections import deque
 
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
 
-def bfs(graph, start, k, visited):
-    queue = deque([start])
-    visited[start] = True
-    cnt =0
-    while queue and k>cnt:
-        if k ==1:
-            return 
-        v = queue.popleft()
-        cnt+=1
-        for i in graph[v]:
-            if not visited[v]:
-                queue.append(i)
-                cnt+=1
-                visited[i] = True
-
-
-# 도시 n , 도로 m , 거리 k, 출발 x
-n, m, k, x = map(int, sys.stdin.readline().split(" "))
-graph = []
 for _ in range(m):
-    a, b = map(int, sys.stdin.readline().split(" "))
+    a, b = map(int, input().split())
     graph[a].append(b)
-    visited = []
-    bfs(graph, x, k, visited)
+distance = [-1] * (n + 1)
+distance[x] = 0
+
+
+def bfs(start):
+    q = deque([x])
+    while q:
+        now = q.popleft()
+        for next_node in graph[now]:
+            if distance[next_node] == -1:  # 아직 방문 X 도시라면
+                distance[next_node] = distance[now] + 1
+                q.append(next_node)
+
+
+bfs(x)
+check = False
+for i in range(1, n + 1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if not check:
+    print(-1)
