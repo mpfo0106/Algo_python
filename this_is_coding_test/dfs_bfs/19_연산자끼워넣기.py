b4dee1f17a6b18652573ccb8210729db2bc5f718
mpfo0106 <mpfo0106@gmail.com>
2023-12-01 20:22:37 +0900

    큰수의 법칙

---
+++ this_is_coding_test/dfs_bfs/19_연산자끼워넣기.py
@@ -0,0 +1,8 @@
+# 만들 수 있는 식의 최대, 최소 구하기
+# 연산자는 최대 100개
+# 최대값이 나오려면
+
+import sys
+n = int(sys.stdin.readline())
+nums = list(map(int,sys.stdin.readline().split()))
+ops = map(int,sys.stdin.readline().split())
---
+++ this_is_coding_test/greedy/실전문제2_큰수의 법칙.py
@@ -0,0 +1,24 @@
+# 중복인 수는 k번까지 더할수있음
+# 또 같은 수가 있어도 중복으로 보지 않음
+# n 은 자연수 개수
+# m은 곱해야하는 개수
+# k는 limit
+
+import sys
+
+n, m, k = map(int, sys.stdin.readline().split())
+list = list(map(int, sys.stdin.readline().split()))
+list.sort()
+flag = 0
+ans = 0
+while m > 0:
+    if flag == k:
+        ans += list[-2]
+        flag = 0
+    else:
+        ans += list[-1]
+        flag += 1
+    m -= 1
+
+print(ans)
+#TODO 역순 정렬 할필요없이 list[-1] [-2] 로 치자
