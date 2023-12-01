d9f1ce025e45dec46566f7d2878d4816bdfd1352
mpfo0106 <mpfo0106@gmail.com>
2023-12-01 20:47:01 +0900

    실전문제3_숫자카드게임

---
+++ this_is_coding_test/greedy/실전문제3_숫자카드게임.py
@@ -0,0 +1,20 @@
+#TODO // O
+
+# 행을 선택
+# 가장 숫자가 낮은 카드를 뽑기
+# 최종적으로 가장 높은 숫자의 카드를 뽑을 수 있게 전략짜기
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+data = []
+maxi = 0
+for _ in range(n):
+    line = list(map(int, sys.stdin.readline().split()))
+    line.sort()
+    data.append(line)
+for i in range(n):
+    maxi = max(maxi, data[i][0])
+print(maxi)
+
+#TODO sort 안하고 min으로 바로 받아도돼
