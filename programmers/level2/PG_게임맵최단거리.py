3df49c490fb47f23b30cae1f00d50e97f84dd103
mpfo0106 <mpfo0106@gmail.com>
2024-01-03 17:19:33 +0900

    PG_게임맵최단거리

--- programmers/level2/PG_게임맵최단거리.py
+++ programmers/level2/PG_게임맵최단거리.py
@@ -1,18 +1,29 @@
-# 흰색만 밟아.
+# 흰색 1 만 밟아.
 # 최단경로, 가중치 없음 => bfs
 #
 from collections import deque
+
 maps = [[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]
+
+cnt = 0
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
 def solution(maps):
-    answer = 0
-    return answer
+    n, m = len(maps), len(maps[0])
+    q = deque()
+    q.append((0, 0))
 
-def bfs():
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
+                maps[nx][ny] = maps[x][y] + 1
+                q.append((nx, ny))
+    return maps[n-1][m-1]
 
-
-
-
-
-solution()
+print(solution(maps))
